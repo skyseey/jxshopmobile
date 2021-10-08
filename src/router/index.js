a1bf8feda8c4ef55f617c3d6b09007a89d42748e
yellow-sky <9379290+yellow-sky@user.noreply.gitee.com>
2021-10-08 19:27:22 +0800

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,17 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
+  // 这里添加各自的路由
   {
     path: "/",
     name: "Home",
     component: () => import("../views/Home.vue"),
   },
-  {
-    path: "/Home1",
-    name: "Home1",
-    component: () => import("../views/Home1.vue"),
-  },
+  // {
+  //   path: "/Home1",
+  //   name: "Home1",
+  //   component: () => import("../views/Home1.vue"),
+  // },
 ];
 
 const router = new VueRouter({
