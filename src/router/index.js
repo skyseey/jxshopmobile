7515a38bb7731d9825a49620fcdf0db6c63d7085
丁大壮 <254906823@qq.com>
2021-10-09 11:05:00 +0800

    购物车+文章

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,17 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+/*
+ * @Author: your name
+ * @Date: 2021-10-08 19:49:47
+ * @LastEditTime: 2021-10-09 09:50:07
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \jxshopmobile\src\router\index.js
+ */
+import Vue from "vue"
+import VueRouter from "vue-router"
+import list from "../views/list.vue"
+import orders from "../views/orders.vue"
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   // 这里添加各自的路由
@@ -10,15 +20,27 @@ const routes = [
     name: "Home",
     component: () => import("../views/Home.vue"),
   },
+  {
+    path: "/list",
+    name: "list",
+    component: () => import("../views/list.vue"),
+  },
+  {
+    path: "/orders",
+    name: "orders",
+    component: () => import("../views/orders.vue"),
+  },
   // {
   //   path: "/Home1",
   //   name: "Home1",
   //   component: () => import("../views/Home1.vue"),
   // },
-];
+]
 
 const router = new VueRouter({
   routes,
-});
+  list,
+  orders,
+})
 
-export default router;
+export default router
---
+++ src/views/orders.vue
@@ -0,0 +1,39 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-10-08 19:49:47
+ * @LastEditTime: 2021-10-09 10:48:33
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \jxshopmobile\src\views\orders.vue
+-->
+<template>
+  <div class="orders">
+    <P>购物车</P>
+    <img
+      src="https://imgt1.oss-cn-shanghai.aliyuncs.com/ecAllRes/images/noCart.png"
+      alt=""
+    />
+    <van-submit-bar :price="3050" button-text="提交订单" @submit="onSubmit">
+      <van-checkbox v-model="checked">全选</van-checkbox>
+    </van-submit-bar>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/css/common.scss";
+.orders {
+  p {
+    line-height: px2rem(40);
+    text-align: center;
+  }
+  img {
+    width: px2rem(129);
+    height: px2rem(129);
+    margin: px2rem(129) px2rem(129) px2rem(129) px2rem(129);
+  }
+}
+</style>
