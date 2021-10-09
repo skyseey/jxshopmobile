30ddc2a207283f3057c0e8b0f04fab7b7ed3fd9a
kirinwu <1270314454@qq.com>
2021-10-09 14:54:54 +0800

    2021/10/9 14:54

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+
 import Show from "@/views/Show.vue";
 import List from "@/views/List.vue";
-import Cats from "@/views/Cats.vue";
-import Logins from "@/views/Logins.vue";
+import Cates from "@/views/Cates.vue";
+import Login from "@/views/Login.vue";
 import Orders from "@/views/Orders.vue";
 import Users from "@/views/Users.vue";
 
@@ -18,8 +19,8 @@ const routes = [
     children: [
       { path: "/show", alias: "/", component: Show }, //首页
       { path: "/list", component: List }, //列表页
-      { path: "/cats", component: Cats }, //购物车
-      { path: "/logins", component: Logins }, //登录页
+      { path: "/cats", component: Cates }, //购物车
+      { path: "/login", component: Login }, //登录页
       { path: "/orders", component: Orders }, //订单
       { path: "/users", component: Users }, //用户
     ],
